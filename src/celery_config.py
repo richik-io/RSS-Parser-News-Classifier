9b9f613fe185131fe7c2290cbf27fe9cb95165da
richik-io <126912928+richik-io@users.noreply.github.com>
2024-10-08 14:04:45 +0530

    Update celery_config.py

--- src/celery_config.py
+++ src/celery_config.py
@@ -3,7 +3,7 @@ from celery import Celery
 # Create a Celery instance
 app = Celery('news_processing', broker='redis://localhost:6379/0')
 
-# Optional configuration
+
 app.conf.task_routes = {
     'process_article': {'queue': 'article_processing_queue'},
 }
